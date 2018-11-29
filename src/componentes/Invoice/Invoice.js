ae57498a470a59819845beec575fae9604bc3016
Luis Rodriguez <CodeKommissar@Gmail.com>
2018-11-29 16:32:10 -0500

    Añadir contenido del componente

--- src/componentes/Invoice/Invoice.js
+++ src/componentes/Invoice/Invoice.js
@@ -6,7 +6,7 @@ class Invoice extends Component {
   componentDidMount() {
     $('[data-toggle="tooltip"]').tooltip();
   }
-  
+
   componentDidUpdate() {
     $('[data-toggle="tooltip"]').tooltip();
   }
@@ -16,16 +16,44 @@ class Invoice extends Component {
       <div className="Invoice">
         <div className="container">
           <h2>
-            Invoice 
-            <span 
+            Invoice
+            <span
               className="invoice-number"
-              data-toggle="tooltip" 
-              data-placement="top" 
+              data-toggle="tooltip"
+              data-placement="top"
               title="Invoice Number"
             >
               S03834754
             </span>
           </h2>
+          <div className="card">
+            <div className="card-header encabezado-carta">
+              Invoice - Revision
+            </div>
+            <div className="card-body">
+              <div className="card-group">
+                <div className="card">
+                  <div className="card-body">
+                    <p>Invoice # <span className="valor">33-0092kk00</span></p>
+                    <p>Date <span className="valor">7/31/18</span></p>
+                    <p>Due Date <span className="valor">7/31/18</span></p>
+                    <p>Terms <span className="valor">Net 0</span></p>
+                  </div>
+                </div>
+                <div className="card">
+                  <div className="card-body">
+                    <h5 className="card-title">Bill To</h5>
+                  </div>
+                </div>
+                <div className="card">
+                  <div className="card-body">
+                    <h5 className="card-title">New Balance upon Re-booking</h5>
+                    <p className="nuevo-balance">$452.45</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     );
