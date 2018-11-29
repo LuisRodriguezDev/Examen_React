41e695805518a05f10517afc518b2ce40687c4e6
Luis Rodriguez <CodeKommissar@Gmail.com>
2018-11-29 15:58:30 -0500

    Añadir heading del componente

--- src/componentes/Invoice/Invoice.js
+++ src/componentes/Invoice/Invoice.js
@@ -1,12 +1,35 @@
-import React from 'react';
+import React, { Component } from 'react';
+import $ from 'jquery';
 import './Invoice.css';
 
-const Invoice = () => {
-  return (
-    <div className="Invoice">
-      
-    </div>
-  );
-};
+class Invoice extends Component {
+  componentDidMount() {
+    $('[data-toggle="tooltip"]').tooltip();
+  }
+  
+  componentDidUpdate() {
+    $('[data-toggle="tooltip"]').tooltip();
+  }
+
+  render() {
+    return (
+      <div className="Invoice">
+        <div className="container">
+          <h2>
+            Invoice 
+            <span 
+              className="invoice-number"
+              data-toggle="tooltip" 
+              data-placement="top" 
+              title="Invoice Number"
+            >
+              S03834754
+            </span>
+          </h2>
+        </div>
+      </div>
+    );
+  }
+}
 
 export default Invoice;
